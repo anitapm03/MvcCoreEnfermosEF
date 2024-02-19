8387a84baca8d86ed1eb9f259d68804331617155
anitapm03 <apereiramolinero@gmail.com>
2024-02-19 11:04:40 +0100

    Add project files.

---
+++ MvcCoreEnfermosEF/Repositories/RepositoryEnfermo.cs
@@ -0,0 +1,22 @@
+﻿using MvcCoreEnfermosEF.Data;
+using MvcCoreEnfermosEF.Models;
+
+namespace MvcCoreEnfermosEF.Repositories
+{
+    public class RepositoryEnfermo
+    {
+        private EnfermoContext context;
+
+        public RepositoryEnfermo(EnfermoContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Enfermo> GetEnfermos()
+        {
+            var consulta = from datos in this.context.Enfermos
+                           select datos;
+            return consulta.ToList();
+        }
+    }
+}
